c07835db9bdfa52c8e95e341a10a0cdda15d6646
Fenrigald <tishur227@mail.ru>
2022-06-21 19:42:49 +0300

    add gitignore

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('contacts/', views.ContactsView.as_view()),
     path('courses_list/', views.CoursesListView.as_view()),
     path('doc_site/', views.DocSiteView.as_view()),
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name="index"),
     path('login/', views.LoginView.as_view()),
     path('news/', views.NewsView.as_view()),
 ]
